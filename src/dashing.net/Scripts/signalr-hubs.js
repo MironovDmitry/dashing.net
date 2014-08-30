34edef5af228c7eeab61f980c93531eefe9d5d34
Neil Edwards <nje@seventyfour.co.uk>
2014-08-30 07:14:25 +0100

    Added the script to auto reconnect the client should it disconnect. All this is detailed at http://www.asp.net/signalr/overview/signalr-20/hubs-api/handling-connection-lifetime-events. The particular section this script came from is under the "How to continuously reconnect" heading.

--- src/dashing.net/Scripts/signalr-hubs.js
+++ src/dashing.net/Scripts/signalr-hubs.js
@@ -1,8 +1,17 @@
 ﻿(function ($) {
-    $.ajax({
-        url: "signalr/hubs",
-        dataType: "script",
-        async: false,
-        cache: true
-    });
+	$.ajax({
+		url: "signalr/hubs",
+		dataType: "script",
+		async: false,
+		cache: true
+	});
+
+	$.connection.hub.disconnected(function () {
+		setTimeout(function () {
+			$.connection.hub.start();
+		}, 5000); // Restart connection after 5 seconds.
+	});
+
+	$.connection.hub.start();
 }(jQuery));
+
