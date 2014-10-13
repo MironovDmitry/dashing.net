853c5bdcf83be1b44daab75a4e47cced9ea35c89
Neil Edwards <nje@seventyfour.co.uk>
2014-10-13 12:49:43 +0100

    Inclusion of json2 for use with signalR. https://github.com/SignalR/SignalR/wiki/Faq

--- src/dashing.net/App_Start/BundleConfig.cs
+++ src/dashing.net/App_Start/BundleConfig.cs
@@ -1,48 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
-using dashing.net.Infrastructure;
-
-namespace dashing.net.App_Start
+﻿namespace dashing.net.App_Start
 {
-    public class BundleConfig
-    {
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            var application = new ScriptBundle("~/bundles/application-js")
-                .Include("~/Scripts/jquery-{version}.js")
-                .Include("~/Scripts/jquery.signalR-{version}.js")
-                .Include("~/Scripts/signalr-hubs.js")
-                .Include("~/Scripts/es5-shim.js")
-                .Include("~/Scripts/batman.js")
-                .Include("~/Scripts/batman.jquery.js")
-                .Include("~/Scripts/dashing.coffee")
-                .Include("~/Scripts/jquery.leanModal.min.js")
-                .Include("~/Scripts/jquery.knob.js")
-                .Include("~/Scripts/d3.v3.js")
-                .Include("~/Scripts/rickshaw.js")
-                .Include("~/Scripts/jquery.gridster.js")
-                .Include("~/Scripts/dashing.gridster.coffee")
-                .IncludeDirectory("~/Widgets", "*.coffee", true)
-                .Include("~/Scripts/application.coffee")
-                .Include("~/Scripts/highcharts.js");
+	using System.Web.Optimization;
+	using dashing.net.Infrastructure;
 
-            application.EnableFileExtensionReplacements = false;
-            application.Transforms.Add(new CoffeeTransform());
-            bundles.Add(application);
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			var application = new ScriptBundle("~/bundles/application-js")
+				.Include("~/Scripts/jquery-{version}.js")
+				.Include("~/Scripts/jquery.signalR-{version}.js")
+				.Include("~/Scripts/signalr-hubs.js")
+				.Include("~/Scripts/es5-shim.js")
+				.Include("~/Scripts/batman.js")
+				.Include("~/Scripts/batman.jquery.js")
+				.Include("~/Scripts/dashing.coffee")
+				.Include("~/Scripts/jquery.leanModal.min.js")
+				.Include("~/Scripts/jquery.knob.js")
+				.Include("~/Scripts/d3.v3.js")
+				.Include("~/Scripts/rickshaw.js")
+				.Include("~/Scripts/jquery.gridster.js")
+				.Include("~/Scripts/dashing.gridster.coffee")
+				.IncludeDirectory("~/Widgets", "*.coffee", true)
+				.Include("~/Scripts/application.coffee")
+				.Include("~/Scripts/highcharts.js")
+				.Include("~/Scripts/json2.js");
 
-            var styles = new Bundle("~/bundles/application-css")
-                .Include("~/Content/css/font-awesome.css")
-                .Include("~/Content/jquery.gridster.css")
-                .IncludeDirectory("~/Widgets", "*.scss", true)
-                .Include("~/Content/application.scss");
+			application.EnableFileExtensionReplacements = false;
+			application.Transforms.Add(new CoffeeTransform());
+			bundles.Add(application);
 
-            styles.Transforms.Add(new ScssTransform());
-            bundles.Add(styles);
+			var styles = new Bundle("~/bundles/application-css")
+				.Include("~/Content/css/font-awesome.css")
+				.Include("~/Content/jquery.gridster.css")
+				.IncludeDirectory("~/Widgets", "*.scss", true)
+				.Include("~/Content/application.scss");
 
-            BundleTable.EnableOptimizations = true;
-        }
-    }
+			styles.Transforms.Add(new ScssTransform());
+			bundles.Add(styles);
+
+			BundleTable.EnableOptimizations = true;
+		}
+	}
 }
